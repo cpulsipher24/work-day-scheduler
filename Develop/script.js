ae9683c248d15661050e8e9f51a1a42d55d21f77
Cpulsipher24 <collintpulsipher@gmail.com>
2023-12-20 20:39:41 -0700

    Pushing starter code to my respository

--- Develop/script.js
+++ Develop/script.js
@@ -19,5 +19,5 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // TODO: Add code to display the current date in the header of the page..
 });
